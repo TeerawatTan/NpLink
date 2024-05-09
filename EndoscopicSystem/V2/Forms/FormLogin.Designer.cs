d8d7c240f0bd639778cc6e86fa8b0511f71e7d18
TEERAWAT TANATAN (Outsource) <OUTSOURCE1893@setth.onmicrosoft.com>
2024-05-09 16:37:08 +0700

    Adjust from v.2.3.1 to new v.2.3.2

--- EndoscopicSystem/V2/Forms/FormLogin.Designer.cs
+++ EndoscopicSystem/V2/Forms/FormLogin.Designer.cs
@@ -127,7 +127,7 @@ namespace EndoscopicSystem.V2.Forms
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(295, 17);
             this.label3.TabIndex = 100;
-            this.label3.Text = "Copyright © 2020 All Rights Reserved V.2.3.1";
+            this.label3.Text = "Copyright © 2020 All Rights Reserved V.2.3.2";
             // 
             // FormLogin
             // 
